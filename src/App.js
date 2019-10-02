13eeda0b858a09034710600bc36ec46b3cb9194c
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-10-01 23:11:07 -0300

    aula react03

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Index  from './pages/home/Index';
 
-class Test extends Component {
-    render() {
-       
-    return <button onClick={() =>this.props.change(this.props.text)}>{this.props.text}</button>
-           
-    }
-}
-
-class TestTitle extends Component{
-    render(){
-        return <h2> Primeira Página {this.props.title}</h2>
-    }
-}
+import './template/assets/css/normalize.css';
+import './template/assets/css/template.css';
+import './template/assets/css/style.css';
 export default class App extends Component{
-    constructor(props){  //serve para passar estados
-      super(props);
-      this.state = {
-        btnAtual: ''
-      }
-    }
-      //function handleChange
-    handleChange = async(nome) => {
-    await  this.setState({
-        btnAtual: nome
-      });
+    render(){
+        return (
+            <Index />
+        )
     }
 
-
-  render(){ 
-    console.log(this.state);
-    return(
-      <React.Fragment>
-        <TestTitle title={ this.state.btnAtual } />
-        <Test change={ this.handleChange } id="01" text='opção 01'/>
-        <Test change={ this.handleChange } id="02" text='opção 02'/>
-        <Test change={ this.handleChange } id="03" text='opção 03'/>
-      </React.Fragment>
-    ) 
-
-  }
+    
 }
